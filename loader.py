104887c09f267310a2e28bdfb83945e70a1164e6
Zoe Fisher <zoefisher87@icloud.com>
2024-04-27 22:35:11 -0600

    added new datasets to loader experiments

--- loader.py
+++ loader.py
@@ -8,11 +8,10 @@ def get_dataset_names(experiment):
     experiment_datasets = {
         "feminism_incel": ["feminism_full", "incels_full"],
         "feminism_over_time": ["feminism_2015_2017", "feminism_2017_2019", "feminism_2019_2021", "feminism_2021_2023"],
-        "within_feminism": ["feminism_full", "fourthwavewomen"], # add intersectionality, intersectionalfems, and radicalfeminism
-        "within_incels": ["incels", "braincels", "trufemcels", "mensrights"], # add theredpill
+        "within_feminism": ["feminism_full", "fourthwavewomen", "blackladies", "feminisms", "feminismuncensored", "women", "fireyfemmes"], 
         # same as within incels due to migration across subreddits over time
         "incels_over_time": ["incels", "braincels", "trufemcels", "mensrights"], # add theredpill
     }
     if experiment not in experiment_datasets:
-        return ["incels", "braincels", "trufemcels", "mensrights", "incels_full","feminism_full", "feminism_2015_2017", "feminism_2017_2019", "feminism_2019_2021", "feminism_2021_2023"]
+        return ["incels", "braincels", "trufemcels", "mensrights", "incels_full","feminism_full", "feminism_2015_2017", "feminism_2017_2019", "feminism_2019_2021", "feminism_2021_2023", "fourthwavewomen", "blackladies", "feminisms", "feminismuncensored", "women", "fireyfemmes"]
     return experiment_datasets[experiment]
